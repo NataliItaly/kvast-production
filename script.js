b2544ce902f910efb4c1012720ee699f0511ab95
Krylova Nataliya <natalinebo2014@yandex.ru>
2022-01-16 22:40:38 +0100

    feat: add js feature

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ BURGER.addEventListener('click', function() {
 
 const LINKS = document.querySelectorAll('.nav-links');
 
-function closeMenu(event) {
-    if (event.target.classList.contains('nav-link')) {
+/* function closeMenu(event) {
+    if (event.target.classList.contains('nav-links')) {
         MENU.classList.remove('open');
         BURGER.classList.remove('active');
     }
-  }    
+  }   */  
+
+  MENU.addEventListener('click', function() {
+    if (BURGER.classList.contains('active') && MENU.classList.contains('open')) {
+      BURGER.classList.remove('active');
+    MENU.classList.remove('open');
+    }
+    
+}); 
